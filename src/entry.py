181b0eb61243fd38edfe7f2e62f6d94367146346
Blankpanda <Blankpanda8@gmail.com>
2015-10-29 22:32:18 -0400

    researched and tested types for a get function. look at source

--- src/entry.py
+++ src/entry.py
@@ -2,34 +2,28 @@ import sys
 import getpass
 
 def get_username():
-
+    # gets the reddit username of the user
     while True:
-
         username = input("Enter username: ")
-
         if username != "":
             return "" + username
         else:
             continue
 
 def get_password():
-
+    # gets the reddit password associated with the username
     while True:
-
         password = getpass.getpass("Enter password: ")
-
         if password != "":
             return password
         else:
             continue
 
 def get_subreddit_name():
-
+    # gets the name of the subreddit thats being queried
     while True:
-
-    subreddit = input("Enter subreddit name: ")
-
-    if subreddit != "":
-        return subreddit
-    else:
-        continue
+        subreddit = input("Enter subreddit name: ")
+        if subreddit != "":
+            return subreddit
+        else:
+            continue
--- src/reddit.py
+++ src/reddit.py
@@ -15,7 +15,11 @@ get_limit = 1
 
 r.login(username, password, disable_warning=True)
 
+#user = r.get_redditor(username)
+#print(user.link_karma + user.comment_karma)
+
 submissions = r.get_subreddit(subreddit).get_hot(limit = get_limit)
 submissions = next(submissions)
+# research praw.objects.permissions in https://github.com/praw-dev/praw
 
-print(submissions)
+print (type(submissions))
