60a5d6c28374a8085290c9d93d9b438eb8f28967
Blankpanda <Blankpanda8@gmail.com>
2015-11-02 19:17:33 -0500

    image sorting in folders

--- src/reddit.py
+++ src/reddit.py
@@ -1,24 +1,27 @@
-import praw
-import sys
-import urllib
-
+import praw, sys, urllib, re, os
 from entry import *
 
+request_limit = 1
 
-r = praw.Reddit(user_agent="Test Script by /u/blankpanda")
 
-username = get_username()
-password = get_password()
+r = praw.Reddit(user_agent="Awwnime Image Grabber for Reddit /u/blankpanda")
+
+#username = get_username()
+#password = get_password()
 subreddit_name = get_subreddit_name()
 
-request_limit = 10
 
-r.login(username, password, disable_warning=True)
+#r.login(username, password, disable_warning=True)
 
-submissions = r.get_subreddit(subreddit_name).get_top(limit = request_limit)
+submissions = r.get_subreddit(subreddit_name).get_hot(limit = request_limit)
 
-count = 0
-
+# downloads images
 for submission in submissions:
-    urllib.request.urlretrieve(submission.url, "test" + str(count) + ".jpg")
-    count += 1
+    tag = re.search("\[(.*?)\]", submission.title)
+    folder_name = tag.group(0)
+
+
+    os.makedirs(folder_name)
+
+    urllib.request.urlretrieve(submission.url,
+    folder_name + "/" + "img_" + str(submission.id) + ".jpg")
