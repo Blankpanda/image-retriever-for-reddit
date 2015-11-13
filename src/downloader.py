ca3dd1af563971a0976b19c5ec02ac35eb85ac4c
Blankpanda <Blankpanda8@gmail.com>
2015-11-13 13:57:42 -0500

    working on removing files that aren't images

--- src/downloader.py
+++ src/downloader.py
@@ -4,47 +4,95 @@ bad_characters = ["<", ">", ":", "/", "\\", "|", "?", "*" , '"']
 file_types = ["jpg", "png", "gif"]
 
 def download_image(url, folder_name, id):
+
     file_type = get_file_type(url)
     url = add_file_type(url)
-    for character in bad_characters:
+
+    for character in bad_characters: # the tag has bad characters in it
         if character in folder_name:
             folder_name = folder_name.replace(character, "")
+
     os.makedirs(folder_name)
-    urllib.request.urlretrieve(url, folder_name + "/"
-    + "img_" + str(id) + file_type)
+
+    try:
+        print("downloading " + url)
+        urllib.request.urlretrieve(url, folder_name + "/"
+        + "img_" + str(id) + file_type)
+    except Exception as e:
+        # os.remove(path_name)
+        print(url + "is not an image.  Skipping.")
 
 def download_image_append(url,folder_name,id):
+
     file_type = "." + get_file_type(url)
     url = add_file_type(url)
-    urllib.request.urlretrieve(url, folder_name + "/"
-    + "img_" + str(id) + file_type)
+
+    try:
+        print("downloading " + url)
+        urllib.request.urlretrieve(url, folder_name + "/" + "img_"
+        + str(id) + file_type)
+    except Exception as e:
+        print(url + "is not an image.  Skipping.")
+
 
 def download_image_tagless(url,id):
+
     file_type = "." + get_file_type(url)
     url = add_file_type(url)
+
     folder_name = "tagless"
+
     os.makedirs(folder_name)
-    urllib.request.urlretrieve(url, folder_name + "/"
-    + "img_" + str(id) + file_type)
+
+    try:
+        print("downloading " + url)
+        urllib.request.urlretrieve(url, older_name + "/"
+        + "img_" + str(id) + file_type)
+    except Exception as e:
+        print(url + "is not an image.  Skipping.")
 
 
 def download_image_tagless_append(url,id):
+
     file_type = "." + get_file_type(url)
     url = add_file_type(url)
+
     folder_name = "tagless"
-    urllib.request.urlretrieve(url, folder_name + "/"
-    + "img_" + str(id) + file_type)
+
+
+    try:
+        print("downloading " + url)
+        urllib.request.urlretrieve(url, folder_name + "/" + "img_"
+        + str(id) + file_type)
+    except Exception as e:
+        print(url + "is not an image.  Skipping")
+
 
 def download_album_image(url,id, index):
+
+    print("in album: " + id)
+
     file_type = "." + get_file_type(url)
+
     os.makedirs(str(id))
-    urllib.request.urlretrieve(url, id + "/"
-    + str(index) + file_type)
+
+    try:
+        print(" downloading " + url )
+        urllib.request.urlretrieve(url, id + "/" + str(index) + file_type)
+    except Exception as e:
+        print(" " + url + "is not an image.  Skipping") # this should never happen.
+
 
 def download_album_image_append(url,id, index):
+
     file_type = "." + get_file_type(url)
-    urllib.request.urlretrieve(url, id + "/"
-    + str(index) + file_type)
+
+    try:
+        print(" downloading: " + url)
+        urllib.request.urlretrieve(url, url, id + "/" + str(index) + file_type)
+    except Exception as e:
+        print(" " + url + "   is not an image.  Skipping") # again this should never happen
+
 
 def get_file_type(url):
     if ".png" in url:
